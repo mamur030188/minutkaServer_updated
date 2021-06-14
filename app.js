9e68443d265872d525a37233463109ba1814046b
Mamirjon Mamadaliev <mike030188@gmail.com>
2021-06-14 22:44:08 +0900

    File Upload

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
 
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
 
 const mongoose = require('mongoose');
 
@@ -62,6 +63,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
